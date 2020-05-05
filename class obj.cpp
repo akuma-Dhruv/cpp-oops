10956ff4e186bc2cb913fd73e617cb3b06dfc90b
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-05-05 22:30:37 +0530

    obj

---
+++ class obj.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class people
+{       public:
+    string naam,pta;
+    int m;
+
+
+};
+
+
+int main (void){
+
+people p;
+people *p1= new people;
+
+
+p.m=12;//static allocation 
+
+(*p1).m=12;//dynamic allocationn
+p1->m = 122;//another way to allocate dynamically
+
+int y=5;
+cout<<
+p.m;
+cout<<
+}
