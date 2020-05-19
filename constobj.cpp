4198b6faf5fa278d40724f43136fa2fab98b6205
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-05-19 21:41:36 +0530

    constant object and functions

--- constobj.cpp
+++ constobj.cpp
@@ -17,8 +17,39 @@ int main() {
     s1.age = 20;
     Student const s2 = s1;
 
-    cout << s2.rollNumber << " " << s2.age;
+    cout << s2.rollNumber << " " << s2.age;// will give 101 20 as output
 }
 
 
 //s2 will create a copy of s1
+
+//////////////////////////////////////////////////////////////
+
+class Student {
+    int rollNumber;
+
+    public :
+
+    int age;
+
+    Student(int r) {
+        rollNumber = r;
+    }
+
+    int getRollNumber() const {                 ///will genetrate a error if "cost" is removed as you cant call a non cinstant by a constant obj
+                                                
+        return rollNumber;
+    }
+
+};
+
+
+int main() {
+    Student s1(101);
+    s1.age = 20;
+
+    Student const s2 = s1;
+    cout << s2.getRollNumber();
+    cout << s1.getRollNumber();//will work perfectly despite of being constant function 
+}
+//similar example 
