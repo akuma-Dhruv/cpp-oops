b90fb992b804f8369ee11c1753b9feee35b7d263
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-05-19 21:33:52 +0530

    const obj

---
+++ constobj.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Student {
+    public :
+
+    int rollNumber;
+    int age;
+
+};
+
+
+int main() {
+    Student s1;
+
+
+    s1.rollNumber = 101;
+    s1.age = 20;
+    Student const s2 = s1;
+
+    cout << s2.rollNumber << " " << s2.age;
+}
+
+
+//s2 will create a copy of s1
